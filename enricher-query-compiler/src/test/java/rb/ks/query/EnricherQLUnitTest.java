1231098fa7926addcb5ed5052d09c36172281cf1
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2016-10-11 17:01:25 +0200

    Added EnricherQL unit tests

---
+++ enricher-query-compiler/src/test/java/rb/ks/query/EnricherQLUnitTest.java
@@ -0,0 +1,72 @@
+package rb.ks.query;
+
+import org.antlr.v4.runtime.ANTLRInputStream;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.junit.Test;
+import rb.ks.query.compiler.EnricherQLLexer;
+import rb.ks.query.compiler.EnricherQLParser;
+
+public class EnricherQLUnitTest {
+
+    @Test
+    public void InsertIntoShouldWork() {
+
+        String query = "INSERT INTO STREAM rb_output";
+
+        ANTLRInputStream inputStream = new ANTLRInputStream(query);
+        EnricherQLLexer lexer = new EnricherQLLexer(inputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        EnricherQLParser parser = new EnricherQLParser(tokens);
+
+        parser.query_output();
+
+    }
+
+    @Test
+    public void JoinShouldWork() {
+
+        String query = "JOIN SELECT * FROM STREAM rb_input USING joiner.package.Class";
+
+        ANTLRInputStream inputStream = new ANTLRInputStream(query);
+        EnricherQLLexer lexer = new EnricherQLLexer(inputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        EnricherQLParser parser = new EnricherQLParser(tokens);
+
+        parser.query_join();
+
+    }
+
+    @Test
+    public void SimpleSelectShouldWork() {
+
+        String query = "SELECT * FROM STREAM rb_input " +
+                "JOIN SELECT a,b FROM STREAM rb_input2 USING joiner.package.Class " +
+                "INSERT INTO STREAM rb_output";
+
+        ANTLRInputStream inputStream = new ANTLRInputStream(query);
+        EnricherQLLexer lexer = new EnricherQLLexer(inputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        EnricherQLParser parser = new EnricherQLParser(tokens);
+
+        parser.query();
+
+    }
+
+    @Test
+    public void ComplexSelectShouldWork() {
+
+        String query = "SELECT * FROM STREAM rb_input " +
+                "JOIN SELECT a,b,c FROM STREAM rb_input2 USING joiner.package.Class1 " +
+                "JOIN SELECT * FROM TABLE rb_input3 USING joiner.pkg1.pkg2.Class2 "+
+                "JOIN SELECT x,y FROM STREAM rb_input4 USING joiner.Class3 " +
+                "INSERT INTO TABLE rb_output";
+
+        ANTLRInputStream inputStream = new ANTLRInputStream(query);
+        EnricherQLLexer lexer = new EnricherQLLexer(inputStream);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        EnricherQLParser parser = new EnricherQLParser(tokens);
+
+        parser.query();
+
+    }
+}
