3686a64e71cf1c200ea0987ccb7cbf0f8b335679
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2016-10-13 12:25:30 +0200

    First ks implementation

---
+++ service/src/main/java/rb/ks/join/Joiner.java
@@ -0,0 +1,17 @@
+package rb.ks.join;
+
+import org.apache.kafka.streams.kstream.ValueJoiner;
+import org.omg.CORBA.Object;
+
+import java.util.Map;
+
+public abstract class Joiner<V1, V2> implements
+        ValueJoiner<V1, V2, Map<String, Object>> {
+
+    public abstract Map<String, Object> join(V1 stream, V2 table);
+
+    @Override
+    public Map<String, Object> apply(V1 value1, V2 value2) {
+        return join(value1, value2);
+    }
+}
--- service/src/main/java/rb/ks/model/PlanModel.java
+++ service/src/main/java/rb/ks/model/PlanModel.java
@@ -1,10 +1,27 @@
 package rb.ks.model;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import rb.ks.exceptions.PlanBuilderException;
+import rb.ks.model.antlr4.Query;
+
+import java.util.*;
 
 public class PlanModel {
+    Map<String, Query> queries = new LinkedHashMap<>();
 
+    @JsonCreator
+    public PlanModel(@JsonProperty("queries") Map<String, String> queries) {
+        queries.forEach((name, queryString) -> {
+            Query query = null; //TODO: Implement query parser!
+            this.queries.put(name, query);
+        });
+    }
 
+    @JsonProperty
+    public Map<String, Query> getQueries() {
+        return queries;
+    }
 
     public void validate() throws PlanBuilderException {
     }
