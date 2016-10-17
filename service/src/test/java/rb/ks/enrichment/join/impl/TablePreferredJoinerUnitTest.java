5cc2b105a7962408ce7c6e7cb5de659f3c0ffa6c
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2016-10-17 12:50:39 +0200

    Fix name

--- service/src/test/java/rb/ks/enrichment/join/impl/TablePreferredJoinerUnitTest.java
+++ service/src/test/java/rb/ks/enrichment/join/impl/TablePreferredJoinerUnitTest.java
@@ -8,7 +8,7 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
-public class TablePreferredJoinerTest {
+public class TablePreferredJoinerUnitTest {
 
     TablePreferredJoiner tablePreferredJoiner;
 
