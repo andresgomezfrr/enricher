73c39221d8ba3a9b93cdef80693adfac5e6f7a92
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2016-10-19 10:38:32 +0200

    Add MacVendorEnrich

---
+++ extensions/src/main/java/rb/ks/enrichment/MacVendorEnrich.java
@@ -0,0 +1,86 @@
+package rb.ks.enrichment;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import rb.ks.enrichment.simple.BaseEnrich;
+import rb.ks.metrics.MetricsManager;
+
+import java.io.*;
+import java.util.HashMap;
+import java.util.Map;
+
+import static rb.ks.enrichment.utils.Constants.__CLIENT_MAC;
+import static rb.ks.enrichment.utils.Constants.__CLIENT_MAC_VENDOR;
+
+public class MacVendorEnrich extends BaseEnrich {
+
+    private static final Logger log = LoggerFactory.getLogger(MacVendorEnrich.class);
+
+    public static String ouiFilePath;
+    public Map<String, String> ouiMap;
+
+    @Override
+    public void prepare(Map<String, Object> properties, MetricsManager metricsManager) {
+        ouiFilePath = properties.containsKey("oui.file.path") ? String.valueOf(properties.get("oui.file.path")) : "/opt/rb/etc/objects/mac_vendors";
+        ouiMap = new HashMap<>();
+
+        InputStream in = null;
+
+        try {
+            in = new FileInputStream(ouiFilePath);
+        } catch (FileNotFoundException e) {
+            log.error("The MacVendor file couldn't be found", e);
+        }
+
+        if(in != null) {
+            InputStreamReader isr = new InputStreamReader(in);
+            BufferedReader br = new BufferedReader(isr);
+
+            try {
+                String line = br.readLine();
+
+                while(line != null) {
+                    String[] tokens = line.split("\\|");
+
+                    if(tokens.length == 2)
+                        ouiMap.put(tokens[0], tokens[1]);
+
+                    line = br.readLine();
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public Map<String, Object> enrich(Map<String, Object> message) {
+        Map<String, Object> vendorMap = new HashMap<>();
+        vendorMap.putAll(message);
+
+        String clientMac = String.valueOf(message.get(__CLIENT_MAC));
+
+        if (clientMac != null) {
+            String oui = buildOui(clientMac);
+
+            if (ouiMap.get(oui) != null) {
+                vendorMap.put(__CLIENT_MAC_VENDOR, ouiMap.get(oui));
+            }
+
+        }
+
+        return vendorMap;
+    }
+
+    private String buildOui(Object object) {
+        String mac = object.toString();
+        mac = mac.trim().replaceAll("[-:]", "");
+        return mac.substring(0, 6).toUpperCase();
+    }
+
+    @Override
+    public void stop() {
+
+    }
+}
