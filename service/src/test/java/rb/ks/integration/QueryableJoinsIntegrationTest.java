55e61d1602ee6e53d64c9f0ae053dc1f61ac38db
Juan Jesús Chorro Bergillo <jjchorrobe@gmail.com>
2016-10-19 12:45:55 +0200

    Added QueryableJoins integration test

---
+++ service/src/test/java/rb/ks/integration/QueryableJoinsIntegrationTest.java
@@ -0,0 +1,179 @@
+package rb.ks.integration;
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.streams.KeyValue;
+import org.apache.kafka.streams.StreamsConfig;
+import org.apache.kafka.streams.integration.utils.EmbeddedSingleNodeKafkaCluster;
+import org.apache.kafka.streams.integration.utils.IntegrationTestUtils;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.ClassRule;
+import org.junit.Test;
+import rb.ks.builder.Builder;
+import rb.ks.builder.config.Config;
+import rb.ks.serializers.JsonDeserializer;
+import rb.ks.serializers.JsonSerde;
+import rb.ks.serializers.JsonSerializer;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+
+import static org.apache.kafka.streams.StreamsConfig.APPLICATION_ID_CONFIG;
+import static org.junit.Assert.assertEquals;
+import static rb.ks.builder.config.Config.ConfigProperties.BOOTSTRAPER_CLASSNAME;
+
+public class QueryableJoinsIntegrationTest {
+
+
+    @ClassRule
+    public static final EmbeddedSingleNodeKafkaCluster CLUSTER = new EmbeddedSingleNodeKafkaCluster();
+
+    private static final int REPLICATION_FACTOR = 1;
+
+    private static final String INPUT_FLOW_TOPIC = "flow";
+    private static final String INPUT_LOCATION_TOPIC = "location";
+    private static final String INPUT_REPUTATION_TOPIC = "reputation";
+
+    private static final String QUERYABLE_TOPIC = "__enricher_queryable";
+
+    private static final String OUTPUT_TOPIC = "output";
+
+    private static Properties producerConfig = new Properties();
+
+    private static Properties consumerConfig = new Properties();
+
+    @BeforeClass
+    public static void startKafkaCluster() throws Exception {
+        CLUSTER.createTopic(INPUT_FLOW_TOPIC, 2, REPLICATION_FACTOR);
+        CLUSTER.createTopic(INPUT_LOCATION_TOPIC, 2, REPLICATION_FACTOR);
+        CLUSTER.createTopic(INPUT_REPUTATION_TOPIC, 2, REPLICATION_FACTOR);
+
+        CLUSTER.createTopic(QUERYABLE_TOPIC, 2, REPLICATION_FACTOR);
+
+        CLUSTER.createTopic(OUTPUT_TOPIC, 2, REPLICATION_FACTOR);
+
+
+        producerConfig.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        producerConfig.put(ProducerConfig.ACKS_CONFIG, "all");
+        producerConfig.put(ProducerConfig.RETRIES_CONFIG, 0);
+        producerConfig.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, Serdes.String().serializer().getClass());
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+
+
+        consumerConfig.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        consumerConfig.put(ConsumerConfig.GROUP_ID_CONFIG, "test-consumer");
+        consumerConfig.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        consumerConfig.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+    }
+
+    @Test
+    public void queryableJoinsShouldWork() throws Exception {
+        Map<String, Object> streamsConfiguration = new HashMap<>();
+
+        String appId = UUID.randomUUID().toString();
+        streamsConfiguration.put(APPLICATION_ID_CONFIG, appId);
+        streamsConfiguration.put(StreamsConfig.ZOOKEEPER_CONNECT_CONFIG, CLUSTER.zKConnectString());
+        streamsConfiguration.put(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, CLUSTER.bootstrapServers());
+        streamsConfiguration.put(StreamsConfig.KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass().getName());
+        streamsConfiguration.put(StreamsConfig.VALUE_SERDE_CLASS_CONFIG, JsonSerde.class.getName());
+        streamsConfiguration.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        streamsConfiguration.put(StreamsConfig.NUM_STREAM_THREADS_CONFIG, 1);
+
+        producerConfig.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        consumerConfig.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+
+        Config configuration = new Config(streamsConfiguration);
+        configuration.put("file.bootstraper.path", Thread.currentThread().getContextClassLoader().getResource("queryable-joins-integration-test.json").getFile());
+        configuration.put(BOOTSTRAPER_CLASSNAME, "rb.ks.builder.bootstrap.FileBootstraper");
+
+        Builder builder = new Builder(configuration);
+
+        Map<String, Object> ipMessage = new HashMap<>();
+        ipMessage.put("ip", "1.2.3.4");
+
+        KeyValue<String, Map<String, Object>> kvIpMessage = new KeyValue<>("MAC_A", ipMessage);
+
+        Map<String, Object> locMessage = new HashMap<>();
+        locMessage.put("loc", "X, Y");
+
+        KeyValue<String, Map<String, Object>> kvLocMessage = new KeyValue<>("MAC_A", locMessage);
+
+        IntegrationTestUtils.produceKeyValuesSynchronously(INPUT_LOCATION_TOPIC, Collections.singletonList(kvLocMessage), producerConfig);
+
+        IntegrationTestUtils.produceKeyValuesSynchronously(INPUT_FLOW_TOPIC, Collections.singletonList(kvIpMessage), producerConfig);
+
+        Map<String, Object> expectedData = new HashMap<>();
+        expectedData.put("ip", "1.2.3.4");
+        expectedData.put("loc", "X, Y");
+
+        KeyValue<String, Map<String, Object>> expectedOutputKv = new KeyValue<>("MAC_A", expectedData);
+
+        List<KeyValue<String, Map>> receivedMessagesFromOutput = IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_TOPIC, 1);
+
+        // STEP 1: CHECK THAT RECEIVED MESSAGE NOT CONTAINS REPUTATION INFORMATION (ONLY FLOW AND LOCATION)
+        assertEquals(Collections.singletonList(expectedOutputKv), receivedMessagesFromOutput);
+
+        List<KeyValue<String, Map>> receivedQueryableMessage = IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig, QUERYABLE_TOPIC, 1);
+
+        Map<String, Object> expectedQueryableMessage = new HashMap<>();
+        expectedQueryableMessage.put("joiner", "streamQueryablePreferred");
+        expectedQueryableMessage.put("type", "joiner-query");
+        expectedQueryableMessage.put("table", "reputation");
+
+        KeyValue<String, Map<String, Object>> expectedQueryableKv = new KeyValue<>("MAC_A", expectedQueryableMessage);
+
+        // STEP 2: CHECK THAT __enricher_queryable RECEIVED MESSAGE
+        assertEquals(Collections.singletonList(expectedQueryableKv), receivedQueryableMessage);
+
+        Map<String, Object> repMessage = new HashMap<>();
+        repMessage.put("rep", 15);
+
+        KeyValue<String, Map<String, Object>> kvRepMessage = new KeyValue<>("MAC_A", repMessage);
+
+        IntegrationTestUtils.produceKeyValuesSynchronously(INPUT_REPUTATION_TOPIC, Collections.singletonList(kvRepMessage), producerConfig);
+
+        IntegrationTestUtils.produceKeyValuesSynchronously(INPUT_LOCATION_TOPIC, Collections.singletonList(kvLocMessage), producerConfig);
+
+        IntegrationTestUtils.produceKeyValuesSynchronously(INPUT_FLOW_TOPIC, Collections.singletonList(kvIpMessage), producerConfig);
+
+        receivedMessagesFromOutput = IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig, OUTPUT_TOPIC, 1);
+
+        expectedData.put("rep", 15);
+
+        // STEP 3: CHECK THAT RECEIVED MESSAGE CONTAINS COMPLETE INFORMATION (FLOW, LOCATION and REPUTATION)
+        assertEquals(Collections.singletonList(expectedOutputKv), receivedMessagesFromOutput);
+
+        // STEP 4: CHECK THAT __enricher_queryable NOT RECEIVED ANY MESSAGE
+        IntegrationTestUtils.waitUntilMinKeyValueRecordsReceived(consumerConfig, QUERYABLE_TOPIC, 0);
+
+        builder.close();
+    }
+
+    @AfterClass
+    public static void stopKafkaCluster() {
+        CLUSTER.stop();
+    }
+
+    public static String getFileContent(File file) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+
+        StringBuilder stringBuffer = new StringBuilder();
+
+        String line;
+
+        while ((line = bufferedReader.readLine()) != null) {
+
+            stringBuffer.append(line).append("\n");
+        }
+
+        return stringBuffer.toString();
+    }
+
+}
