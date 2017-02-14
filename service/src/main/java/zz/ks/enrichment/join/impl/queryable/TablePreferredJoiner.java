04f3dc88c170c6e9d5c327aab07b88191815c4e7
Andres Gomez Ferrer <andresgomezfrr@gmail.com>
2017-02-14 11:22:11 +0200

    Refactor packages

---
+++ service/src/main/java/zz/ks/enrichment/join/impl/queryable/TablePreferredJoiner.java
@@ -0,0 +1,17 @@
+package zz.ks.enrichment.join.impl.queryable;
+
+import zz.ks.enrichment.join.QueryableJoiner;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TablePreferredJoiner  extends QueryableJoiner<Map<String, Object>> {
+
+    @Override
+    public Map<String, Object> join(Map<String, Object> stream, Map<String, Object> table) {
+        Map<String, Object> joinerMap = new HashMap<>();
+        if (stream != null) joinerMap.putAll(stream);
+        if (table != null) joinerMap.putAll(table);
+        return joinerMap;
+    }
+}
