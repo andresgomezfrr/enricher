594426c18f320ecf68a8f53f1d8baabe988058a2
Pablo Casares <lito92vforce@gmail.com>
2018-06-07 12:46:28 +0200

    WIZZIE-1920 Update enricher repo to log4j2

--- docker/build/enricher-start.sh
+++ docker/build/enricher-start.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 envsubst < /opt/enricher/config/config_env.json > /opt/enricher/config/config.json
+envsubst < /opt/enricher/config/log4j2_env.xml > /opt/enricher/config/log4j2.xml
 /opt/enricher/bin/enricher-start.sh /opt/enricher/config/config.json
